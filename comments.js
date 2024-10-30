b45b7db871a9f14c0f238079a9e5aa0bf99e93c0
eaff <eaff.pt@gmail.com>
2024-10-30 18:45:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,8 @@
-// create web server
+//Create web server
 var express = require('express');
 var app = express();
 var fs = require('fs');
 
-// create web server
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
